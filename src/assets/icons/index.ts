ca42924e1ff831b5fb7c4a52eb11bc8d76ac99fd
Jane50 <yuanmei1210@gmail.com>
2026-03-15 17:41:48 +0900

    feat: add sort icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,12 +3,14 @@ import exit from './exit.svg'
 import left from './left.svg'
 import right from './right.svg'
 import back from './back.svg'
+import info from './info.svg'
 
 import Home from './home.svg?react'
 import Map from './map.svg?react'
 import Tag from './tag.svg?react'
 import Day from './day.svg?react'
 import Night from './night.svg?react'
+import Sort from './sort.svg?react'
 
 
 export {
@@ -22,4 +24,6 @@ export {
   left,
   right,
   back,
+  Sort,
+  info,
 }
